9566df0e03b1b59d7beb9168a50b0bffa7fca53c
unknown <mawue2801@gmail.com>
2024-03-27 16:33:01 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Record(BaseModel):
+    parameter1: str
+    parameter2: str
+    parameter3: str
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/add_record/")
+async def add_record(record: Record):
+    # Here you can process the received data, for example, save it to a database
+    # For simplicity, let's just print the received data and return it along with the success message
+    print("Received Record:")
+    print(record)
+    return {"message": "Record added successfully", "record": record.dict()}
